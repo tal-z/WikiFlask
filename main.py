e9b8214e7095984f94f27a5f16bae19867acf82a
talzaken <talzaken@gmail.com>
2021-01-23 13:50:24 -0500

    Working on new interactive layout using Jinja

--- main.py
+++ main.py
@@ -278,6 +278,66 @@ def plot_wiki_editors():
 
 
 
+@app.route('/PlotWikiEditors_JINJA')
+def PlotWikiEditors_JINJA():
+    return render_template('PlotWikiEditors_JINJA.html')
+
+@app.route('/plot_wiki_editors_JINJA')
+def plot_wiki_editors_JINJA():
+    page_title = request.args.get('page_title')
+    page_title = page_title[0].upper() + page_title[1:]
+    try:
+        title = f'Most Frequent Editors of the "{page_title.title()}" Wikipedia Page (Top 10)'
+        user_edits_list = user_edits(page_title).items()
+        user_edits_list_len = len(user_edits_list)
+        count = 0
+        color_count = 0
+        colors = [ '#320E3B', '#FF499E', '#A480CF', '#1E2EDE', '#47A8BD',  '#53FF45',  '#EFCA08',  '#FB6107', '#EC0B43', '#89043D']
+        plt.clf()
+
+        plt.xlabel('Time')
+        plt.ylabel('Revisions count')
+        plt.xticks(rotation=60)
+        user_colors = []
+
+        for entry in user_edits_list:
+            timestamps = entry[1]
+            timestamps.reverse()
+
+            dates = [datetime.strptime(d, '%Y-%m-%dT%H:%M:%SZ') for d in timestamps]
+
+            plt.plot()
+
+            if count >= user_edits_list_len - 10:
+                plt.plot_date(dates, range(len(dates)), linestyle='solid', marker='.', markersize=1.25, label=entry[0], color=colors[color_count])
+                user_colors.append((entry[0], colors[color_count]))
+                color_count += 1
+            else:
+                plt.plot_date(dates, range(len(dates)), linestyle='solid', marker='.', markersize=1.25, label='_nolegend_', color='#D8DDDE')
+
+            count += 1
+
+        #handles, labels = plt.gca().get_legend_handles_labels()
+        #plt.legend(handles[::-1], labels[::-1], loc='upper left', ncol=1, bbox_to_anchor=(1.05, 1), borderaxespad=0.)
+
+
+        plt.tight_layout()
+        #plt.gcf().subplots_adjust(top=.8)
+
+
+        stream = BytesIO()
+        plt.savefig(stream, format='png')
+        stream.seek(0)
+
+        pngImageB64String = "data:image/png;base64,"
+        pngImageB64String += b64encode(stream.getvalue()).decode('utf8')
+
+        print(user_colors)
+
+        return render_template('PlotWikiEditors_JINJA.html', image=pngImageB64String, user_colors=reversed(user_colors), title=title)
+    except:
+        return render_template('PlotWikiEditors_JINJA.html', image='https://upload.wikimedia.org/wikipedia/commons/a/a0/Font_Awesome_5_regular_frown.svg', user_colors=reversed(user_colors), title=title)
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
